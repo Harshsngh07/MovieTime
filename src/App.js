3b815dbeab9fac05cb11e4e7e495a89f353b61f4
Harsh Singh <harshsngh07@gmail.com>
2020-05-21 20:37:48 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.css";
+import Movie from "./assets/movie.png";
 import SearchMovies from "./SearchMovies";
+
 function App() {
   return (
     <div className="container">
+      <img className="logo" src={Movie} alt="movieTime" />
       <h1 className="title">MovieTime</h1>
       <SearchMovies />
     </div>
